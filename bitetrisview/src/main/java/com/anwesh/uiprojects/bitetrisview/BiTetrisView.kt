a9be8609325922d2ea5ccec2a4af91de5d254002
Anwesh43 <anweshthecool0@gmail.com>
2018-09-24 09:39:07 +0530

    adding BiTetris class

--- bitetrisview/src/main/java/com/anwesh/uiprojects/bitetrisview/BiTetrisView.kt
+++ bitetrisview/src/main/java/com/anwesh/uiprojects/bitetrisview/BiTetrisView.kt
@@ -116,4 +116,27 @@ class BiTetrisView(ctx : Context) : View(ctx) {
             next?.draw(canvas, paint)
         }
     }
+
+    data class BiTetris(var i : Int) {
+        private var root : BTNode = BTNode(0)
+        private var curr : BTNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
