444ffbd5acd9964326f556696ad31e778ee44c04
Anwesh43 <anweshthecool0@gmail.com>
2018-09-24 09:25:43 +0530

    adding libraries and defining nodes

---
+++ bitetrisview/src/main/java/com/anwesh/uiprojects/bitetrisview/BiTetrisView.kt
@@ -0,0 +1,14 @@
+package com.anwesh.uiprojects.bitetrisview
+
+/**
+ * Created by anweshmishra on 24/09/18.
+ */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.content.Context
+
+val nodes : Int = 5
